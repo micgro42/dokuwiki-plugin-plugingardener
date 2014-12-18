36b263dd4fa72564e7ecde337edb3411f6917459
Michael Große <grosse@cosmocode.de>
2014-12-18 17:39:53 +0100

    Add lang-file for conf

---
+++ lang/en/settings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * english language file for skilltagicon plugin
+ *
+ * @author Michael Große <grosse@cosmocode.de>
+ */
+
+// keys need to match the config setting name
+$lang['bundledsourcedir'] = 'Download dir?';
+$conf['localdir'] = 'local path to local wiki?';
+
+
+
+//Setup VIM: ex: et ts=4 :
